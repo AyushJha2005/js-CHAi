ae7b96cd6739b528c92543eda08c999403c84d94
AyushJha2005 <ajshc01@gmail.com>
2026-02-02 18:38:29 +0000

    cofigure done

---
+++ pra/variable.js
@@ -0,0 +1,9 @@
+const accountId=142
+let email="aj@gmail.com"
+var pass="1223345"
+account="Ayush"
+// accountId=1
+email="ASH"
+pass="123"
+account="SOMU"
+console.table([accountId,email,pass,account])
